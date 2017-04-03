39a5682eb2c1e50b246eaadd440fda7b7bf92552
Robbert Kauffman <robbert.kauffman@bloomreach.com>
2017-04-03 11:46:19 -0400

    removed unused dependencies and updated versions in bower.json

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('devPanel', [
     'ngMaterial',
-    'ui.tree',
     'ui.ace',
     'componentItem',
     'componentEditor',
